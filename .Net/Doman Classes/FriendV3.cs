bd590376a57e987c881f408e39cdf1edcaf78def
Victor Martinez <victoremartinez18@gmail.com>
2025-02-13 14:50:17 -0500

    Update FriendV3.cs

--- .Net/Doman Classes/FriendV3.cs
+++ .Net/Doman Classes/FriendV3.cs
@@ -1,12 +1,4 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-using System.Xml.Linq;
 
-
-namespace Sabio.Models.Domain.Friends
 {
     public class FriendV3
     {
